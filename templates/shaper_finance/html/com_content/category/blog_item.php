2360373b95654cf24bced68be9dd329738a1f1c5
Tomek Stach <tomasz.stach@gmail.com>
2021-04-17 13:58:26 +0200

    Joomla! Update

--- templates/shaper_finance/html/com_content/category/blog_item.php
+++ templates/shaper_finance/html/com_content/category/blog_item.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package     Joomla.Site
  * @subpackage  Layout
@@ -14,7 +15,7 @@ $tpl_params = JFactory::getApplication()->getTemplate(true)->params;
 JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
 $canEdit = $this->item->params->get('access-edit');
 $info = $params->get('info_block_position', 0);
-$useDefList = ($params->get('show_modify_date') || $params->get('show_publish_date') || $params->get('show_create_date') || $params->get('show_hits') || $params->get('show_category') || $params->get('show_parent_category') || $params->get('show_author') );
+$useDefList = ($params->get('show_modify_date') || $params->get('show_publish_date') || $params->get('show_create_date') || $params->get('show_hits') || $params->get('show_category') || $params->get('show_parent_category') || $params->get('show_author'));
 
 // Post Format
 $post_attribs = new JRegistry(json_decode($this->item->attribs));
@@ -22,44 +23,44 @@ $post_format = $post_attribs->get('post_format', 'standard');
 ?>
 
 <?php if ($this->item->state == 0 || strtotime($this->item->publish_up) > strtotime(JFactory::getDate()) || ((strtotime($this->item->publish_down) < strtotime(JFactory::getDate())) && $this->item->publish_down != JFactory::getDbo()->getNullDate())) :
-    ?>
-    <div class="system-unpublished">
-    <?php endif; ?>
-    <?php
-    $attribs = json_decode($this->item->attribs);
+?>
+  <div class="system-unpublished">
+  <?php endif; ?>
+  <?php
+  $attribs = json_decode($this->item->attribs);
 
-    $link = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language)); ?>
+  $link = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language)); ?>
 
-    <div class="sp-simpleportfolio-overlay-wrapper clearfix">
+  <div class="sp-simpleportfolio-overlay-wrapper clearfix">
 
 
-        <img class="sp-simpleportfolio-img" src="<?php echo $attribs->spfeatured_image;?>" alt="Gastronomia">
+    <img class="sp-simpleportfolio-img" src="<?php echo $attribs->spfeatured_image; ?>" alt="Gastronomia">
 
-        <div class="sp-simpleportfolio-overlay">
-            <div class="sp-vertical-middle">
-                <div>
-                    <div class="sp-simpleportfolio-btns">
-                        <a class="btn-zoom" href="<?php echo $link;?>" data-featherlight="image"><i class="fa fa-photo"></i></a>
-                    </div>
-                </div>
-            </div>
+    <div class="sp-simpleportfolio-overlay">
+      <div class="sp-vertical-middle">
+        <div>
+          <div class="sp-simpleportfolio-btns">
+            <a class="btn-zoom" href="<?php echo $link; ?>" data-featherlight="image"><i class="fa fa-photo"></i></a>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 
-    <div class="sp-simpleportfolio-info">
-        <h3 class="sp-simpleportfolio-title">
-            <a href="<?php echo $link;?>"><?php echo $this->item->title;?></a>
-        </h3>
+  <div class="sp-simpleportfolio-info">
+    <h3 class="sp-simpleportfolio-title">
+      <a href="<?php echo $link; ?>"><?php echo $this->item->title; ?></a>
+    </h3>
 
-        </div>
+  </div>
 
-        <?php if ($this->item->state == 0 || strtotime($this->item->publish_up) > strtotime(JFactory::getDate()) || ((strtotime($this->item->publish_down) < strtotime(JFactory::getDate())) && $this->item->publish_down != JFactory::getDbo()->getNullDate())) :
-            ?>
-    </div>
+  <?php if ($this->item->state == 0 || strtotime($this->item->publish_up) > strtotime(JFactory::getDate()) || ((strtotime($this->item->publish_down) < strtotime(JFactory::getDate())) && $this->item->publish_down != JFactory::getDbo()->getNullDate())) :
+  ?>
+  </div>
 <?php endif; ?>
 
 <?php if ($params->get('show_tags') && !empty($this->item->tags->itemTags)) : ?>
-    <?php echo JLayoutHelper::render('joomla.content.tags', $this->item->tags->itemTags); ?>
+  <?php echo JLayoutHelper::render('joomla.content.tags', $this->item->tags->itemTags); ?>
 <?php endif; ?>
 
 <?php echo $this->item->event->afterDisplayContent; ?>
