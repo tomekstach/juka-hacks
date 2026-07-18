e65136883ad0379aa002fa572256720094bad646
Tomek Stach <tomasz.stach@gmail.com>
2026-07-18 22:12:56 +0200

    Update

--- templates/shaper_finance/html/com_content/category/blog_item.php
+++ templates/shaper_finance/html/com_content/category/blog_item.php
@@ -1,66 +1,81 @@
 <?php
-
 /**
  * @package     Joomla.Site
  * @subpackage  Layout
  *
- * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 defined('_JEXEC') or die;
 
 // Create a shortcut for params.
 $params = $this->item->params;
-$tpl_params = JFactory::getApplication()->getTemplate(true)->params;
+$tpl_params 	= JFactory::getApplication()->getTemplate(true)->params;
 JHtml::addIncludePath(JPATH_COMPONENT . '/helpers/html');
-$canEdit = $this->item->params->get('access-edit');
-$info = $params->get('info_block_position', 0);
-$useDefList = ($params->get('show_modify_date') || $params->get('show_publish_date') || $params->get('show_create_date') || $params->get('show_hits') || $params->get('show_category') || $params->get('show_parent_category') || $params->get('show_author'));
+$canEdit 	 = $this->item->params->get('access-edit');
+$info    	 = $params->get('info_block_position', 0);
+$useDefList = ($params->get('show_modify_date') || $params->get('show_publish_date') || $params->get('show_create_date')
+	|| $params->get('show_hits') || $params->get('show_category') || $params->get('show_parent_category') || $params->get('show_author') );
 
 // Post Format
-$post_attribs = new JRegistry(json_decode($this->item->attribs));
+$post_attribs = new JRegistry(json_decode( $this->item->attribs ));
 $post_format = $post_attribs->get('post_format', 'standard');
+
 ?>
 
-<?php if ($this->item->state == 0 || strtotime($this->item->publish_up) > strtotime(JFactory::getDate()) || ((strtotime($this->item->publish_down) < strtotime(JFactory::getDate())) && $this->item->publish_down != JFactory::getDbo()->getNullDate())) :
+<?php if ($this->item->state == 0 || (!empty($this->item->publish_up) && strtotime($this->item->publish_up) > strtotime(JFactory::getDate())
+	|| (!empty($this->item->publish_down) && strtotime($this->item->publish_down) < strtotime(JFactory::getDate())) && $this->item->publish_down != JFactory::getDbo()->getNullDate())) : ?>
+	<div class="system-unpublished">
+<?php endif; ?>
+
+<?php
+	if($post_format=='standard') {
+		echo JLayoutHelper::render('joomla.content.intro_image', $this->item);
+	} else {
+		echo JLayoutHelper::render('joomla.content.post_formats.post_' . $post_format, array('params' => $post_attribs, 'item' => $this->item));
+	}
 ?>
-  <div class="system-unpublished">
-  <?php endif; ?>
-  <?php
-  $attribs = json_decode($this->item->attribs);
 
-  $link = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language)); ?>
+<div class="blog-content-wrap">
+	<div class="entry-header<?php echo $tpl_params->get('show_post_format') ? ' has-post-format': ''; ?>">
 
-  <div class="sp-simpleportfolio-overlay-wrapper clearfix">
+		<?php echo JLayoutHelper::render('joomla.content.blog_style_default_item_title', $this->item); ?>
+		
+		<?php echo JLayoutHelper::render('joomla.content.post_formats.icons',  $post_format); ?>
+		<?php if ($useDefList && ($info == 0 || $info == 2)) : ?>
+			<?php echo JLayoutHelper::render('joomla.content.info_block.block', array('item' => $this->item, 'params' => $params, 'position' => 'above')); ?>
+		<?php endif; ?>
+	</div>
+</div>
 
 
-    <img class="sp-simpleportfolio-img" src="<?php echo $attribs->spfeatured_image; ?>" alt="Gastronomia">
 
-    <div class="sp-simpleportfolio-overlay">
-      <div class="sp-vertical-middle">
-        <div>
-          <div class="sp-simpleportfolio-btns">
-            <a class="btn-zoom" href="<?php echo $link; ?>" data-featherlight="image"><i class="fa fa-photo"></i></a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
 
-  <div class="sp-simpleportfolio-info">
-    <h3 class="sp-simpleportfolio-title">
-      <a href="<?php echo $link; ?>"><?php echo $this->item->title; ?></a>
-    </h3>
+<?php if ($params->get('show_readmore') && $this->item->readmore) :
+	if ($params->get('access-view')) :
+		$link = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language));
+	else :
+		$menu = JFactory::getApplication()->getMenu();
+		$active = $menu->getActive();
+		$itemId = $active->id;
+		$link1 = JRoute::_('index.php?option=com_users&view=login&Itemid=' . $itemId);
+		$returnURL = JRoute::_(ContentHelperRoute::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language));
+		$link = new JUri($link1);
+		$link->setVar('return', base64_encode($returnURL));
+	endif; ?>
 
-  </div>
+	<?php echo JLayoutHelper::render('joomla.content.readmore', array('item' => $this->item, 'params' => $params, 'link' => $link)); ?>
 
-  <?php if ($this->item->state == 0 || strtotime($this->item->publish_up) > strtotime(JFactory::getDate()) || ((strtotime($this->item->publish_down) < strtotime(JFactory::getDate())) && $this->item->publish_down != JFactory::getDbo()->getNullDate())) :
-  ?>
-  </div>
+<?php endif; ?>
+
+<?php if ($this->item->state == 0 || (!empty($this->item->publish_up) && strtotime($this->item->publish_up) > strtotime(JFactory::getDate())
+	|| (!empty($this->item->publish_down) && strtotime($this->item->publish_down) < strtotime(JFactory::getDate())) && $this->item->publish_down != JFactory::getDbo()->getNullDate())) : ?>
+	<div class="system-unpublished">
 <?php endif; ?>
 
 <?php if ($params->get('show_tags') && !empty($this->item->tags->itemTags)) : ?>
-  <?php echo JLayoutHelper::render('joomla.content.tags', $this->item->tags->itemTags); ?>
+	<?php echo JLayoutHelper::render('joomla.content.tags', $this->item->tags->itemTags); ?>
 <?php endif; ?>
 
 <?php echo $this->item->event->afterDisplayContent; ?>
